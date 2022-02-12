d2e03c29ce16b91ee6d249508a2f3394cf8e4288
Brian Sunter <public@briansunter.com>
2022-02-11 20:17:37 -0800

    fix build

---
+++ infra/lib/infra-stack.d.ts
@@ -0,0 +1,4 @@
+import * as cdk from '@aws-cdk/core';
+export declare class InfraStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps);
+}
---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,23 @@
+import * as cdk from '@aws-cdk/core';
+import * as apprunner from '@aws-cdk/aws-apprunner';
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+new apprunner.Service(this, 'Service', {
+  source: apprunner.Source.fromGitHub({
+    repositoryUrl: 'https://github.com/briansunter/cali',
+    branch: 'master',
+    configurationSource: apprunner.ConfigurationSourceType.API,
+    codeConfigurationValues: {
+      runtime: apprunner.Runtime.PYTHON_3,
+      port: '8000',
+      startCommand: 'poetry run manage.py runserver',
+      buildCommand: 'curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python - && source $HOME/.poetry/env && poetry install',
+    },
+    connection: apprunner.GitHubConnection.fromConnectionArn('arn:aws:apprunner:us-east-1:847136656635:connection/brian-github/9f4abc9e7627446fb76f838ba0ed3fbc'),
+  }),
+});
+    // The code that defines your stack goes here
+  }
+}
